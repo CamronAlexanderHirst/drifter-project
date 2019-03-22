1799618be784e5755aea278899e9250a6f272f71
CamronAlexanderHirst <camron.hirst@colorado.edu>
2019-03-22 11:31:04 -0600

    Start balloon project, deleted git example files used to learn git * start project

---
+++ src/gen_field.py
@@ -0,0 +1,13 @@
+#Function to create an nxm vector field for wind vectors. Outputs an n x m x 2 matrix for x and y wind speeds at each grid point.
+#Alex Hirst
+
+import numpy as np
+
+def generate_field(n, m)
+
+    #Initialize wind matrix
+    Matrix = [[0 for x in range(n)] for y in range(h)]
+
+
+
+
--- src/helloworld.py
+++
@@ -1,4 +0,0 @@
-#Hello world
-
-
-print("hello world")
